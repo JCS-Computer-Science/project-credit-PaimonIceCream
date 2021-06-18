8f18b4384ebde054b76927329357618324539bee
PaimonIceCream <keanu1@educbe.ca>
2021-06-18 17:11:36 -0600

    Final Commit

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -4,6 +4,14 @@ export default class Game extends Phaser.Scene {
     score = 0
     highScore = 0
 
+    directionX
+    directionY
+    rays
+    canDash = true
+    dashDistance = 170
+    invincible = false
+    
+
     /** @type {Phaser.Physics.Arcade.Sprite} */
     player
 
@@ -19,6 +27,9 @@ export default class Game extends Phaser.Scene {
     /** @type {Phaser.Physics.Arcade.World} */
     scoreTimer
 
+    /** @type {Phaser.Physics.Arcade.World} */
+    dashVar
+
     /** @type {Phaser.Types.Input.Keyboard.CursorKeys} */
     cursors
 
@@ -31,12 +42,18 @@ export default class Game extends Phaser.Scene {
     /** @type {Phaser.GameObjects.Text} */
     gameOverText
 
+    /** @type {Phaser.GameObjects.Text} */
+    dashReadyText
+
+
     constructor() {
         super('game')
     }
 
     init() {
         this.score = 0
+        this.canDash = true
+        this.invincible = false
     }
 
     preload() {
@@ -45,6 +62,8 @@ export default class Game extends Phaser.Scene {
         this.load.image('player', 'assets/maroon.png')
         this.load.image('obstacle', 'assets/blue.png')
         this.load.image('instructions', 'assets/instructions.png')
+        this.load.image('poof', 'assets/poof.png')
+        this.load.image('rays', 'assets/rays.png')
 
         this.cursors = this.input.keyboard.createCursorKeys();
     }
@@ -71,7 +90,10 @@ export default class Game extends Phaser.Scene {
             instructions.setVisible(false)
         }, 3600)
 
-        const style = {color:'#2b2926', fontSize: 24}
+        const style = {
+            color: '#2b2926',
+            fontSize: 24
+        }
         const randomVar = this.scale.width - 240
         this.scoreText = this.add.text(10, 10, "Score: 0", style)
         this.highScoreText = this.add.text(randomVar, 10, `Highscore: ${this.highScore}`, style)
@@ -79,25 +101,34 @@ export default class Game extends Phaser.Scene {
         this.scoreTimer = setInterval(() => {
             this.score++
 
-            if(this.score > this.highScore){
+            if (this.score > this.highScore) {
                 this.highScore = this.score
             }
 
             const scoreVar = `Highscore: ${this.highScore}`
-            this.highScoreText.text = scoreVar 
+            this.highScoreText.text = scoreVar
 
             const scoreValue = `Score: ${this.score}`
             this.scoreText.text = scoreValue
         }, 1000)
 
-        
+
+        this.dashReadyText = this.add.text(550, 680, 'Dash Ready', {
+            fontSize: 30,
+            color: '#000000',
+            backgroundColor: '#00FF00'
+        }).setOrigin(0.5)
+
+
+
 
         //obstacle spawn code starts
         const obstacle = this.physics.add.group()
 
         var seconds = 0
         var counter = 0
-        var counterGhost = 0
+        var counterGhostB = 0
+        var counterGhostT = 0
         var intervalLevel = 2.2
 
         var minW = 180
@@ -107,7 +138,8 @@ export default class Game extends Phaser.Scene {
         this.timer = setInterval(() => {
             seconds = seconds + 0.1
             counter = counter + 0.1
-            counterGhost = counterGhost + 0.1
+            counterGhostB = counterGhostB + 0.1
+            counterGhostT = counterGhostT + 0.1
 
             if (Math.round(seconds) == 10) {
                 intervalLevel = 2
@@ -153,20 +185,20 @@ export default class Game extends Phaser.Scene {
                 const x = start
                 const y = Phaser.Math.Between(40 + (height / 2), this.scale.height - (40 + height / 2))
 
-                /** @type {Phaser.Physics.Arcade.Sprite} */     
+                /** @type {Phaser.Physics.Arcade.Sprite} */
                 const object = obstacle.create(x, y, 'obstacle')
                 object.setDisplaySize(width, height)
                 object.setVelocityX(speed)
 
-                /** @type {Phaser.Physics.Arcade.Group}*/       
+                /** @type {Phaser.Physics.Arcade.Group}*/
                 const blueBody = object.body
-                blueBody.updateFromGameObject() 
+                blueBody.updateFromGameObject()
 
                 counter = 0
             }
 
-            //Ghost Block
-            if(counterGhost >= (4.4 + intervalLevel*intervalLevel)){
+            //Ghost Blocks
+            if (counterGhostB >= (4.4 + intervalLevel * intervalLevel)) {
                 const width = Phaser.Math.Between(100, 150)
                 const height = 250 - width
                 var start
@@ -183,26 +215,53 @@ export default class Game extends Phaser.Scene {
                 const x = start
                 const y = Phaser.Math.Between(this.scale.height - (40 + height / 2) - 15, this.scale.height - (40 + height / 2))
 
-                /** @type {Phaser.Physics.Arcade.Sprite} */     
+                /** @type {Phaser.Physics.Arcade.Sprite} */
                 const object = obstacle.create(x, y, 'obstacle')
                 object.setDisplaySize(width, height)
                 object.setVelocityX(speed)
 
-                /** @type {Phaser.Physics.Arcade.Group}*/       
+                /** @type {Phaser.Physics.Arcade.Group}*/
                 const blueBody = object.body
                 blueBody.updateFromGameObject()
-                
-                counterGhost = 0
+
+                counterGhostB = 0
             }
 
+            if (counterGhostT >= (9.5 + intervalLevel * intervalLevel * intervalLevel) && seconds > 25) {
+                const width = Phaser.Math.Between(60, 80)
+                const height = 140 - width
+                var start
+                var speed
+
+                const LR = Phaser.Math.Between(0, 1)
+                if (LR == 0) {
+                    start = 0 - width
+                    speed = velocity
+                } else {
+                    start = this.scale.width + width
+                    speed = -velocity
+                }
+                const x = start
+                const y = Phaser.Math.Between(40 + height / 2, 45 + height / 2)
+
+                /** @type {Phaser.Physics.Arcade.Sprite} */
+                const object = obstacle.create(x, y, 'obstacle')
+                object.setDisplaySize(width, height)
+                object.setVelocityX(speed)
+
+                /** @type {Phaser.Physics.Arcade.Group}*/
+                const blueBody = object.body
+                blueBody.updateFromGameObject()
+
+                counterGhostT = 0
+            }
 
         }, 100)
 
         //obstacle spawn code ends
 
-        
 
-        this.physics.add.collider(
+        this.physics.add.overlap(
             this.player,
             obstacle,
             this.killGame,
@@ -234,28 +293,106 @@ export default class Game extends Phaser.Scene {
             this.player.setVelocityY(780)
         }
 
+        // DASH MECHANICS
+        this.directionX = this.myDirectionX()
+        this.directionY = this.myDirectionY()
+
+        if (this.cursors.space.isDown && this.canDash == true && (this.directionX != 0 || this.directionY != 0)) {
+            this.dashFunction(this.directionX, this.directionY)
+            this.canDash = false
+        }
+
+        if (this.canDash == true) {
+            this.dashReadyText.setVisible(true)
+        } else {
+            this.dashReadyText.setVisible(false)
+        }
+
+        if(this.player.x > this.scale.width){
+            this.player.x = this.scale.width - 70
+        }else if(this.player.x < 0){
+            this.player.x = 80
+        }
+        
+        if(this.player.y > this.scale.height){
+            this.player.y = this.scale.height - 55
+        }else if(this.player.y < 0){
+            this.player.y = 55
+        }
+
     }
 
     killGame(player) {
-        this.player.setVisible(false)
-        this.physics.world.destroy(this.player)
-        this.physics.world.destroy(this.obstacle)
-        clearInterval(this.timer)
-        clearInterval(this.scoreTimer)
+        if (this.invincible == false) {
+            this.player.setVisible(false)
+            this.physics.world.destroy(this.player)
+            this.physics.world.destroy(this.obstacle)
+            clearInterval(this.timer)
+            clearInterval(this.scoreTimer)
+            clearInterval(this.dashVar)
 
-        this.input.keyboard.once('keydown_ENTER', () => {
-            this.scene.start('game')
-        })
+            this.input.keyboard.once('keydown_ENTER', () => {
+                this.scene.start('game')
+            })
 
-        // Game Over Text
-        const gameWidth = this.scale.width * 0.5
-        const gameHeight = this.scale.height * 0.5
+            // Game Over Text
+            const gameWidth = this.scale.width * 0.5
+            const gameHeight = this.scale.height * 0.5
 
-        this.add.text(gameWidth, gameHeight, 'Press Enter to Retry', {
-            fontSize: 40,
-            color: '#000000',
-        }).setOrigin(0.5)
+            this.add.text(gameWidth, gameHeight, 'Press Enter to Retry', {
+                fontSize: 40,
+                color: '#000000',
+            }).setOrigin(0.5)
+        }
+    }
 
+    myDirectionX() {
+        if (this.cursors.right.isDown) {
+            return 1
+        } else if (this.cursors.left.isDown) {
+            return -1
+        } else {
+            return 0
+        }
+    }
+
+    myDirectionY() {
+        if (this.cursors.up.isDown) {
+            return -1
+        } else if (this.cursors.down.isDown) {
+            return 1
+        } else {
+            return 0
+        }
+    }
+
+    dashFunction(x, y) {
+        this.dashVar = setTimeout(() => {
+            this.canDash = true
+        }, 40000)
+        const poof = this.add.image(this.player.x, this.player.y, 'poof').setScale(0.20).setAlpha(0.3)
+
+        this.invincible = true
+
+        const newX = this.player.x + (x * this.dashDistance)
+        const newY = this.player.y + (y * this.dashDistance)
+
+        this.player.setPosition(newX, newY)
+        const rays = this.add.image(this.player.x, this.player.y, 'rays').setScale(0.20).setAlpha(0.5)
+
+        this.player.setVelocity(0, 0)
+        if (y * this.dashDistance < 0) {
+            this.player.setVelocityY(-200)
+        }
+
+        setTimeout(() => {
+            poof.setVisible(false)
+            rays.setVisible(false)
+        }, 200)
+
+        setTimeout(() =>{
+            this.invincible = false
+        }, 500)
     }
 
 }
